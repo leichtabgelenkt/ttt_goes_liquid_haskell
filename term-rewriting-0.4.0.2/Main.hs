048ff4b7e5a9f90ab7e48e4f19a7ba56505bb5dc
csaz8724 <Philipp.Dablander@student.uibk.ac.at>
2024-02-29 21:19:23 +0100

    [ADD] Added Comments for the new functions

--- term-rewriting-0.4.0.2/Main.hs
+++ term-rewriting-0.4.0.2/Main.hs
@@ -115,6 +115,8 @@ definedSymbols rules = temp 1 (Prelude.map head (nub (Prelude.map outermostSymbo
  where temp x [] = []
        temp x y = (head y, head (show x)) : temp (x + 1) (tail y) 
 
+
+-- This function takes the same list of rules twice as input and returns the list of dependency pairs
 dependencyPairs :: [Rule Char Char] ->[Rule Char Char] -> [Rule Char Char]
 dependencyPairs [] _ = []
 dependencyPairs ((Rule lhs rhs) : xs) rules = case drhs of
@@ -124,17 +126,22 @@ dependencyPairs ((Rule lhs rhs) : xs) rules = case drhs of
         dlhs = changeSymbol lhs dSymbols
         drhs = changeSymbolRHS rhs dSymbols
 
+-- Takes a term as a lhs of a rule and a list of Maybe terms as input and constructs a list of rules out of the lhs and all rhs terms which are not Nothing in the list
 composeRules :: Term Char Char -> [Maybe (Term Char Char)] -> [Rule Char Char]
 composeRules x [] = []
 composeRules x (y:ys) = case y of
   Just z -> (Rule x z) : composeRules x ys
   Nothing -> composeRules x ys
 
+-- Takes a term and the list of defined functions and returns a term where the function symbol of the term is changed with the corresponding dependency symbol.
+-- This function is only used for lhs of a dependecy pair, so the function symbol of the term will always be inside the list of defined symbols
 changeSymbol :: Term Char Char -> [(Char, Char)] -> Term Char Char
 changeSymbol (Fun a b) ys = case lookup a ys of
   Just x -> Fun x b
   _ -> Fun a b
 
+-- This function takes a term (which represents a rhs of a dependency pair) and a list of defined symbols. It checks if the outermost function symbol matches one of the defined symbols.
+-- If it does, it gets changed with the corresponding dependency symbol, if it doesn't we do nothing. In both cases we continue with the sub functions.
 changeSymbolRHS ::  Term Char Char -> [(Char, Char)] -> [Maybe (Term Char Char)]
 changeSymbolRHS (Var _) ys = [Nothing]
 changeSymbolRHS (Fun c d) ys = case lookup c ys of
