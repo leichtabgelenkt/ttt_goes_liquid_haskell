38440f1b29b883deb9e14cf9ed5857ee880551fb
Pippo140 <p.dablander@gmx.at>
2024-05-25 20:39:59 +0200

    Some changes

--- term-rewriting-0.4.0.2/Main.hs
+++ term-rewriting-0.4.0.2/Main.hs
@@ -16,6 +16,7 @@ import Data.Function (on, const)
 import Data.Graph
 import Data.Graph.SCC
 import Data.SBV
+import Data.SBV.List
 import GHC.Cmm (CmmNode(res))
 import Data.SBV (constrain, sInteger, allSat)
 import GHC.Prelude (Show(show))
@@ -26,53 +27,53 @@ bbb = sNot aaa
 
 instance (Show f, Show v, Show v') => Show (Reduct f v v') where
   show (Reduct result pos rule subst) =
-    "Reduct { result = " ++ show result
-    ++ ", pos = " ++ show pos
-    ++ ", rule = " ++ show rule
-    ++ ", subst = " ++ show subst
-    ++ " }"
+    "Reduct { result = " Data.List.++ show result
+    Data.List.++ ", pos = " Data.List.++ show pos
+    Data.List.++ ", rule = " Data.List.++ show rule
+    Data.List.++ ", subst = " Data.List.++ show subst
+    Data.List.++ " }"
 
 
 newtype RewriteSequence = RewriteSequence [(Term Char Char, Term Char Char)]
 
 instance Show RewriteSequence where
-  show s@(RewriteSequence a) = show $ Prelude.map listToStringWithArrows $ nub $ removeSublists $ removeEmptySublists $ Prelude.map nub $ showRewriteSequence s (fst (head a))
+  show s@(RewriteSequence a) = show $ Prelude.map listToStringWithArrows $ nub $ removeSublists $ removeEmptySublists $ Prelude.map nub $ showRewriteSequence s (fst (Data.List.head a))
 
 listToStringWithArrows :: [Term Char Char] -> String
 listToStringWithArrows [] = ""
 listToStringWithArrows [x] = show x
-listToStringWithArrows (x:xs) = show x ++ " -> " ++ listToStringWithArrows xs
+listToStringWithArrows (x:xs) = show x Data.List.++ " -> " Data.List.++ listToStringWithArrows xs
 
 
 showRewriteSequence :: RewriteSequence -> Term Char Char -> [[Term Char Char]]
 showRewriteSequence (RewriteSequence []) x = [[]]
-showRewriteSequence (RewriteSequence ((start, end):xs)) x = if start == x then [start : end : t | t <- splitAndRemove $ getL end xs] ++ showRewriteSequence (RewriteSequence xs) x else showRewriteSequence (RewriteSequence xs) x -- ++ showRewriteSequence (RewriteSequence xs)
---[([start, end] ++ getL end xs)]
+showRewriteSequence (RewriteSequence ((start, end):xs)) x = if start == x then [start : end : t | t <- splitAndRemove $ getL end xs] Data.List.++ showRewriteSequence (RewriteSequence xs) x else showRewriteSequence (RewriteSequence xs) x -- Data.List.++ showRewriteSequence (RewriteSequence xs)
+--[([start, end] Data.List.++ getL end xs)]
 
 getL :: Term Char Char -> [(Term Char Char, Term Char Char)] -> [Term Char Char]
 getL _ [] = [Fun '$' []]
 getL endTerm ((startTerm, nextEndTerm) : remainingPairs)
-  | endTerm == startTerm = [startTerm, nextEndTerm] ++ getL nextEndTerm remainingPairs ++ getL endTerm remainingPairs
+  | endTerm == startTerm = [startTerm, nextEndTerm] Data.List.++ getL nextEndTerm remainingPairs Data.List.++ getL endTerm remainingPairs
   | otherwise = getL endTerm remainingPairs
 
 splitAndRemove :: [Term Char Char] -> [[Term Char Char]]
 splitAndRemove [] = []
-splitAndRemove xs = filter (not . null) $ case break (== Fun '$' []) xs of
+splitAndRemove xs = Data.List.filter (not . Data.List.null) $ case break (== Fun '$' []) xs of
   (before, []) -> [before]
   (before, _ : after) -> before : splitAndRemove after
 
 isSublist :: Eq a => [a] -> [a] -> Bool
-isSublist sublist list = sublist `elem` (tail $ init $ subsequences list)
+isSublist sublist list = sublist `Data.List.elem` (Data.List.tail $ Data.List.init $ subsequences list)
 
 removeEmptySublists :: [[Term a b]] -> [[Term a b]]
-removeEmptySublists = filter (not . null)
+removeEmptySublists = Data.List.filter (not . Data.List.null)
 
 removeSublists :: Eq a => [[a]] -> [[a]]
-removeSublists lists = filter (\x -> all (not . (`isSublist` x)) (delete x lists)) lists
+removeSublists lists = Data.List.filter (\x -> Data.List.all (not . (`isSublist` x)) (delete x lists)) lists
 
 mysequence = RewriteSequence [(Fun 'n' [Fun 'u' [Fun 's' [],Fun 't' []],Fun 's' []],Fun 'u' [Fun 'n' [Fun 's' [],Fun 's' []],Fun 'n' [Fun 't' [],Fun 's' []]]),(Fun 'u' [Fun 'n' [Fun 's' [],Fun 's' []],Fun 'n' [Fun 't' [],Fun 's' []]],Fun 'u' [Fun 's' [],Fun 'n' [Fun 't' [],Fun 's' []]]),(Fun 'u' [Fun 'n' [Fun 's' [],Fun 's' []],Fun 'n' [Fun 't' [],Fun 's' []]],Fun 'u' [Fun 'n' [Fun 's' [],Fun 's' []],Fun 'e' []]),(Fun 'u' [Fun 'n' [Fun 's' [],Fun 's' []],Fun 'e' []],Fun 'u' [Fun 's' [],Fun 'e' []]),(Fun 'u' [Fun 's' [],Fun 'e' []],Fun 's' []),(Fun 'u' [Fun 's' [],Fun 'n' [Fun 't' [],Fun 's' []]],Fun 'u' [Fun 's' [],Fun 'e' []]),(Fun 'u' [Fun 's' [],Fun 'e' []],Fun 's' [])]
 
-startElement = fst $ head [(Fun 'n' [Fun 'u' [Fun 's' [],Fun 't' []],Fun 's' []],Fun 'u' [Fun 'n' [Fun 's' [],Fun 's' []],Fun 'n' [Fun 't' [],Fun 's' []]]),(Fun 'u' [Fun 'n' [Fun 's' [],Fun 's' []],Fun 'n' [Fun 't' [],Fun 's' []]],Fun 'u' [Fun 's' [],Fun 'n' [Fun 't' [],Fun 's' []]]),(Fun 'u' [Fun 'n' [Fun 's' [],Fun 's' []],Fun 'n' [Fun 't' [],Fun 's' []]],Fun 'u' [Fun 'n' [Fun 's' [],Fun 's' []],Fun 'e' []]),(Fun 'u' [Fun 'n' [Fun 's' [],Fun 's' []],Fun 'e' []],Fun 'u' [Fun 's' [],Fun 'e' []]),(Fun 'u' [Fun 's' [],Fun 'e' []],Fun 's' []),(Fun 'u' [Fun 's' [],Fun 'n' [Fun 't' [],Fun 's' []]],Fun 'u' [Fun 's' [],Fun 'e' []]),(Fun 'u' [Fun 's' [],Fun 'e' []],Fun 's' [])]
+startElement = fst $ Data.List.head [(Fun 'n' [Fun 'u' [Fun 's' [],Fun 't' []],Fun 's' []],Fun 'u' [Fun 'n' [Fun 's' [],Fun 's' []],Fun 'n' [Fun 't' [],Fun 's' []]]),(Fun 'u' [Fun 'n' [Fun 's' [],Fun 's' []],Fun 'n' [Fun 't' [],Fun 's' []]],Fun 'u' [Fun 's' [],Fun 'n' [Fun 't' [],Fun 's' []]]),(Fun 'u' [Fun 'n' [Fun 's' [],Fun 's' []],Fun 'n' [Fun 't' [],Fun 's' []]],Fun 'u' [Fun 'n' [Fun 's' [],Fun 's' []],Fun 'e' []]),(Fun 'u' [Fun 'n' [Fun 's' [],Fun 's' []],Fun 'e' []],Fun 'u' [Fun 's' [],Fun 'e' []]),(Fun 'u' [Fun 's' [],Fun 'e' []],Fun 's' []),(Fun 'u' [Fun 's' [],Fun 'n' [Fun 't' [],Fun 's' []]],Fun 'u' [Fun 's' [],Fun 'e' []]),(Fun 'u' [Fun 's' [],Fun 'e' []],Fun 's' [])]
 
 test2 = getL (Fun 'n' [Fun 'u' [Fun 's' [],Fun 't' []],Fun 's' []]) [(Fun 'n' [Fun 'u' [Fun 's' [],Fun 't' []],Fun 's' []],Fun 'u' [Fun 'n' [Fun 's' [],Fun 's' []],Fun 'n' [Fun 't' [],Fun 's' []]]),(Fun 'u' [Fun 'n' [Fun 's' [],Fun 's' []],Fun 'n' [Fun 't' [],Fun 's' []]],Fun 'u' [Fun 's' [],Fun 'n' [Fun 't' [],Fun 's' []]]),(Fun 'u' [Fun 'n' [Fun 's' [],Fun 's' []],Fun 'n' [Fun 't' [],Fun 's' []]],Fun 'u' [Fun 'n' [Fun 's' [],Fun 's' []],Fun 'e' []]),(Fun 'u' [Fun 'n' [Fun 's' [],Fun 's' []],Fun 'e' []],Fun 'u' [Fun 's' [],Fun 'e' []]),(Fun 'u' [Fun 's' [],Fun 'e' []],Fun 's' []),(Fun 'u' [Fun 's' [],Fun 'n' [Fun 't' [],Fun 's' []]],Fun 'u' [Fun 's' [],Fun 'e' []]),(Fun 'u' [Fun 's' [],Fun 'e' []],Fun 's' [])]
 test = showRewriteSequence mysequence startElement
@@ -87,7 +88,7 @@ splitString (x:xs) = [x] : splitString xs
 
 -- Gets all function symbols in a set of rules
 collectFunctionsInRuleList :: [Rule Char Char] -> [String]
-collectFunctionsInRuleList rules = nub (splitString (concatMap (\rule -> TermOps.funs (lhs rule) ++ TermOps.funs (rhs rule)) rules))
+collectFunctionsInRuleList rules = nub (splitString (Data.List.concatMap (\rule -> TermOps.funs (lhs rule) Data.List.++ TermOps.funs (rhs rule)) rules))
 
 -- Gets all possible hirachies of function symbols in a set of rules
 getAllFamilies :: [Rule Char Char] -> [[String]]
@@ -101,13 +102,13 @@ outermostSymbol (Fun f _) = [f]
 
 -- Returns a list of the combined outermost symbols of two terms
 compareOutermostSymbols :: Term Char Char -> Term Char Char -> [String]
-compareOutermostSymbols term1 term2 = splitString (outermostSymbol term1 ++ outermostSymbol term2)
+compareOutermostSymbols term1 term2 = splitString (outermostSymbol term1 Data.List.++ outermostSymbol term2)
 
 -- Removes all orderings of a family where the ordering is not the same as in the first argument, which is a sub family
 findOrderings :: [String] -> [[String]] -> [[String]]
 findOrderings _ [] = []
 findOrderings [x, y] allOrderings =
-  filter (\ordering -> isBefore ordering x y) allOrderings
+  Data.List.filter (\ordering -> isBefore ordering x y) allOrderings
   where
     isBefore :: Eq a => [a] -> a -> a -> Bool
     isBefore ordering a b =
@@ -118,7 +119,7 @@ findOrderings [x, y] allOrderings =
 -- Makes a sub function order out of two terms and then refines the family of orderings 
 refine :: [[String]] -> Term Char Char -> [Term Char Char] -> [[String]]
 refine rules t1 [] = []
-refine rules t1 t2 = findOrderings (compareOutermostSymbols t1 (head t2)) rules
+refine rules t1 t2 = findOrderings (compareOutermostSymbols t1 (Data.List.head t2)) rules
 
 -- Just checks if a list is empty or not
 mySat :: [[a]] -> Bool
@@ -129,9 +130,9 @@ mySat _ = True
 rest :: [Rule Char Char] -> Term Char Char -> RewriteSequence
 rest rules term = p ([term], (getAllFamilies rules)) rules
 
--- Adds the head element to the result and then hands the work down
+-- Adds the Data.List.head element to the result and then hands the work down
 p :: ([Term Char Char], [[String]]) -> [Rule Char Char] -> RewriteSequence
-p (terms, families) rules = RewriteSequence (help2 (head terms) families rules rules)
+p (terms, families) rules = RewriteSequence (help2 (Data.List.head terms) families rules rules)
 
 -- Checks if a rules is applicable on a certain term. If so, then it returns the reduct, otherwise it returns an empty list
 getNewReduct :: Term Char Char -> Rule Char Char -> [Term Char Char]
@@ -141,18 +142,18 @@ getNewReduct term rule =
     (reduct:_) -> [result reduct]
 
 -- Here is where most of the work for the REST algorithm happens. This function takes a term, a family of function orderings, the whole list of rewrite rules and the list of rewrite rules which have not been tested yet.
--- If all rules have been tested then it returns an empty list. If the next rules isn't applicable on the term, then a recursive call happens with the tail of the rule list. If the rule is applicable, but the arising subordering
+-- If all rules have been tested then it returns an empty list. If the next rules isn't applicable on the term, then a recursive call happens with the Data.List.tail of the rule list. If the rule is applicable, but the arising subordering
 -- of functions isn't mySatisfiable for the given family, then the result will be ignored and process will be continued with the next rule. However, if everything works out, then the reduct gets into the result list and we get two new
--- function calls: The first one on the old term with the tail of the rules and the second one on the new term with all rules
+-- function calls: The first one on the old term with the Data.List.tail of the rules and the second one on the new term with all rules
 help2 :: Term Char Char -> [[String]] -> [Rule Char Char] ->  [Rule Char Char] -> [(Term Char Char, Term Char Char)]
 help2 term families rules rSet
  | rSet == [] = []
- | null newReduct = help2 term families rules (tail rSet)
- | not (mySat newFamilies) = help2 term families rules (tail rSet)
- | otherwise = [(term, head newReduct)] ++ (help2 term newFamilies rules (tail rSet)) ++ (help2 (head newReduct) newFamilies rules rules)
- where lhs = getLHS (head rSet)
-       rhs = getRHS (head rSet)
-       newReduct = getNewReduct term (head rSet)
+ | Data.List.null newReduct = help2 term families rules (Data.List.tail rSet)
+ | not (mySat newFamilies) = help2 term families rules (Data.List.tail rSet)
+ | otherwise = [(term, Data.List.head newReduct)] Data.List.++ (help2 term newFamilies rules (Data.List.tail rSet)) Data.List.++ (help2 (Data.List.head newReduct) newFamilies rules rules)
+ where lhs = getLHS (Data.List.head rSet)
+       rhs = getRHS (Data.List.head rSet)
+       newReduct = getNewReduct term (Data.List.head rSet)
        newFamilies = refine families lhs [rhs]
 
 getLHS :: Rule Char Char -> Term Char Char
@@ -170,9 +171,9 @@ outermostSymbolRuleRight (Rule _ rhs) = outermostSymbol rhs
 
 -- Returns list of all defined symbols in a set of rules
 definedSymbols :: [Rule Char Char] -> [(Char, Char)]
-definedSymbols rules = temp 1 (Prelude.map head (nub (Prelude.map outermostSymbolRule rules)))
+definedSymbols rules = temp 1 (Prelude.map Data.List.head (nub (Prelude.map outermostSymbolRule rules)))
  where temp x [] = []
-       temp x y = (head y, head (show x)) : temp (x + 1) (tail y)
+       temp x y = (Data.List.head y, Data.List.head (show x)) : temp (x + 1) (Data.List.tail y)
 
 
 -- This function takes the same list of rules twice as input and returns the list of dependency pairs
@@ -180,7 +181,7 @@ dependencyPairs :: [Rule Char Char] -> [Rule Char Char] -> [Rule Char Char]
 dependencyPairs [] _ = []
 dependencyPairs ((Rule lhs rhs) : xs) rules = case drhs of
   [Nothing] -> dependencyPairs xs rules
-  x ->  composeRules dlhs x ++ dependencyPairs xs rules
+  x ->  composeRules dlhs x Data.List.++ dependencyPairs xs rules
   where dSymbols = definedSymbols rules
         dlhs = changeSymbol lhs dSymbols
         drhs = changeSymbolRHS rhs dSymbols
@@ -204,8 +205,8 @@ changeSymbol (Fun a b) ys = case lookup a ys of
 changeSymbolRHS ::  Term Char Char -> [(Char, Char)] -> [Maybe (Term Char Char)]
 changeSymbolRHS (Var _) ys = [Nothing]
 changeSymbolRHS (Fun c d) ys = case lookup c ys of
-  Just x -> Just (Fun x d) : nub (concat [changeSymbolRHS t ys | t <- d])
-  Nothing -> nub (concat [changeSymbolRHS t ys | t <- d])
+  Just x -> Just (Fun x d) : nub (Data.List.concat [changeSymbolRHS t ys | t <- d])
+  Nothing -> nub (Data.List.concat [changeSymbolRHS t ys | t <- d])
 
 -- Given rules, this function returns a list of triple like (Numeration of node, outermost symbol of left side, outermost symbol of right side)
 sccPrepare :: [Rule Char Char] -> Int -> [(Int, String, String)]
@@ -215,7 +216,7 @@ sccPrepare (x:xs) y = (y, outermostSymbolRule x, outermostSymbolRuleRight x) : s
 -- Compute vertices of dependency graph
 getVertices :: [(Int, String, String)] -> [(Int, String, String)] -> [(Int, Int)]
 getVertices [] _ = []
-getVertices (x:xs) y = help x y ++ getVertices xs y
+getVertices (x:xs) y = help x y Data.List.++ getVertices xs y
  where help _ [] = []
        help s@(i, _, r) ((i2, l, _):xs) = if r == l then (i, i2) : help s xs else help s xs
 
@@ -236,7 +237,7 @@ getSccFromDependencyPairs x = sccList graph
        graph = buildG (getMinMax prepare (0,0)) vertices
 
 findRule :: [Rule Char Char] -> (Int, String, String) -> Rule Char Char
-findRule s p@(_, a, b) = if (outermostSymbolRule (head s) == a) && (outermostSymbolRuleRight (head s) == b) then (head s) else findRule (tail s) p
+findRule s p@(_, a, b) = if (outermostSymbolRule (Data.List.head s) == a) && (outermostSymbolRuleRight (Data.List.head s) == b) then (Data.List.head s) else findRule (Data.List.tail s) p
 
 findSccNode :: [Rule Char Char] -> [(Int, String, String)] -> [Int] -> [Rule Char Char]
 findSccNode _ _ [] = []
@@ -264,8 +265,8 @@ subgroup x y
   | otherwise = or [subgroup x v | v <- handBackArgumentsFromTerm y]
 
 isNotProjecting :: Term Char Char -> Projection -> Bool
-isNotProjecting (Fun c _) p = any (\(symbol, list) -> symbol == c && null list) p
-isNotProjecting (Var c) p = any (\(symbol, list) -> symbol == c && null list) p
+isNotProjecting (Fun c _) p = Data.List.any (\(symbol, list) -> symbol == c && Data.List.null list) p
+isNotProjecting (Var c) p = Data.List.any (\(symbol, list) -> symbol == c && Data.List.null list) p
 
 handBackArgumentsFromTerm :: Term Char Char -> [Term Char Char]
 handBackArgumentsFromTerm (Var _) = []
@@ -276,7 +277,7 @@ maybeToInt Nothing = -1
 maybeToInt (Just x) = x
 
 isProjectingToArgument :: Term Char Char -> Term Char Char -> Projection -> Bool
-isProjectingToArgument x ( Fun c vars ) p = maybeToInt (elemIndex x (handBackArgumentsFromTerm s)) + 1 `elem` snd (head (filter (\(symbol, list) -> symbol == c) p))
+isProjectingToArgument x ( Fun c vars ) p = maybeToInt (elemIndex x (handBackArgumentsFromTerm s)) + 1 `Data.List.elem` snd (Data.List.head (Data.List.filter (\(symbol, list) -> symbol == c) p))
   where s = Fun c vars
  
 
@@ -384,7 +385,7 @@ examplerules = [r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15
 drules = dependencyPairs examplerules examplerules
 resultExample = getSccFromDependencyPairs drules
 see = sccPrepare drules 1
-index = [drules !! 7, drules !! 9]
+index = [drules Data.List.!! 7, drules Data.List.!! 9]
 sccTest = getSccFromDependencyPairs (dependencyPairs rulesTest rulesTest)
 findScc = findSccNode drules (sccPrepare drules 1) [7,9]
 
@@ -570,15 +571,25 @@ sampleTerm4 = Fun 'f' [Fun 'g' [Var 'x', Fun 'h' [Fun 'f' [Var 'x', Var 'y'], Va
 resultTerms :: [Reduct Char Char Char]
 resultTerms = fullRewrite rSet sampleTerm4
 
--- try out the Sbv library with an easy example
-s = sat $ do
-  a <- sInteger "a"
-  b <- sInteger "b"
-  constrain $ a + b .== 3
-  constrain $ a .> 0 .&& b .> 0
 
+
+projectionPhilipp :: Term Char Char -> SInteger -> SInteger
+projectionPhilipp (Fun _ xs) x = test `elemAt` x
+  where help (Var b : zs) = read [b] : help zs
+        help [] = []
+        test = literal (help xs)
+
+getNumChildren :: Term Char Char -> Integer
+getNumChildren (Fun _ xs) = toInteger $ Data.List.length xs
 
 main :: IO ()
 main = do
-  solution <- s
-  print solution
+  let s = allSat $ do
+          let sss = Fun 'f' [Var '6', Var '2', Var '3']
+          let t = Fun 'f' [Var '3', Var '2', Var '1']
+          a <- sInteger "a"
+          constrain $ projectionPhilipp sss a .> projectionPhilipp t a
+          constrain $ a .>= 0
+          constrain $ a .< (literal $ getNumChildren sss)
+  result <- s
+  print result
