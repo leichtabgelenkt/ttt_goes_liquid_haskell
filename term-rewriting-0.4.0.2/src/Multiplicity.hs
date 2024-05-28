4728b84a4519a1fac7c2ff026c36e48153cd4e35
Luca Maahs <luca.maahs@outlook.com>
2024-05-28 17:36:15 +0200

    Multiplicity as a Module

---
+++ term-rewriting-0.4.0.2/src/Multiplicity.hs
@@ -0,0 +1,48 @@
+module Multiplicity where
+import Data.Rewriting.Term
+import Data.Rewriting.Term.Type
+import Data.Rewriting.Term.Ops as TermOps
+import Data.Rewriting.Term.Pretty
+import Data.Rewriting.Context
+import Data.Rewriting.CriticalPair
+import Data.Rewriting.Pos
+import Data.Rewriting.Rules
+import Data.Rewriting.Rule
+import Data.Rewriting.Substitution
+import Data.Rewriting.Rule.Type
+import Data.Rewriting.Rules.Rewrite
+import Data.Rewriting.Problem
+import Data.List
+
+type Projection = [(Char, [Int])]
+
+subgroup :: Term Char Char -> Term Char Char -> Bool
+subgroup x y
+  | x == y = True
+  | isVar y = False
+  | otherwise = or [subgroup x v | v <- handBackArgumentsFromTerm y]
+
+isNotProjecting :: Term Char Char -> Projection -> Bool
+isNotProjecting (Fun c _) p = Data.List.any (\(symbol, list) -> symbol == c && Data.List.null list) p
+isNotProjecting (Var c) p = Data.List.any (\(symbol, list) -> symbol == c && Data.List.null list) p
+
+handBackArgumentsFromTerm :: Term Char Char -> [Term Char Char]
+handBackArgumentsFromTerm (Var _) = []
+handBackArgumentsFromTerm (Fun _ args) = args
+
+maybeToInt :: Maybe Int -> Int
+maybeToInt Nothing = -1
+maybeToInt (Just x) = x
+
+isProjectingToArgument :: Term Char Char -> Term Char Char -> Projection -> Bool
+isProjectingToArgument x ( Fun c vars ) p = maybeToInt (elemIndex x (handBackArgumentsFromTerm s)) + 1 `Data.List.elem` snd (Data.List.head (Data.List.filter (\(symbol, list) -> symbol == c) p))
+  where s = Fun c vars
+ 
+
+-- Multiplicity of a term in another termisVar
+multiplicity :: Int -> Term Char Char -> Term Char Char -> Projection -> Int
+multiplicity w s t p
+  | s == t && not (isVar s) = if isNotProjecting t p then w else 0
+  | s == t && isVar s = w
+  | subgroup t s && not (isVar s) = sum [multiplicity w x t p | x <- handBackArgumentsFromTerm s, isProjectingToArgument x s p]
+  | otherwise = 0 -- case4 finished
